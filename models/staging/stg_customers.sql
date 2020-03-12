63423dc3bfa45c19a1ecbf4e164598194dd6ff30
Christine <christinebergersd@gmail.com>
2020-03-12 14:54:15 -0500

    Changed stg_customers to implement best practices (select from base source)

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,5 +1,13 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-from {{ source('shop', 'customers') }}
+with source as (
+  select * from {{ source('shop', 'customers') }}
+),
+
+renamed as (
+  select
+      id as customer_id,
+      first_name,
+      last_name
+  from {{ source('shop', 'customers') }}
+)
+
+select * from renamed
