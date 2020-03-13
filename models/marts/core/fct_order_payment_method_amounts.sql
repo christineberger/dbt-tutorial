45a8d48dc6b24944fd6548824353a7f1d64c511d
Christine <christinebergersd@gmail.com>
2020-03-13 12:39:02 -0500

    Split get_order_payment_methods() in to two macros and added db-utils in packages.yml

---
+++ models/marts/core/fct_order_payment_method_amounts.sql
@@ -0,0 +1,13 @@
+{%- set payment_methods = get_payment_methods() -%}
+
+select
+  order_id,
+
+  {%- for payment_method in payment_methods %}
+  sum(case when payment_method = '{{ payment_method }}' then amount end) as {{ payment_method }}_amount
+  {%- if not loop.last %},{% endif -%}
+  {% endfor %}
+
+  -- sum(amount) as total_amount
+from {{ ref('stg_jaffleshop_payments') }}
+group by 1
